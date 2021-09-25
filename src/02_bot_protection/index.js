33e1a59bf263be7ad581bcbb2a67ae21e848f357
Jorge Cesar <jorge@thecoursekey.com>
2021-09-25 21:39:08 +0200

    first commit

---
+++ src/02_bot_protection/index.js
@@ -0,0 +1,29 @@
+// One gaming company found out that they are losing money because some players are using scripts that are playing the
+// game for players while they are sleeping.
+//
+// From the access logs on their servers, developers can see which commands each player is sending. Analysing them, they
+// discovered those scripts are using the same sequence of commands all the time.
+//
+// Your task is to use cleaned up access logs to discover bots. Cleaned up access log of one player would look like an
+// space separated sequence of commands sent by the player. Detect if 3 sequential commands are repeated at least
+// 2 times from the cleaned up access log.
+//
+// For example, we have following access log:
+//
+// left right fire jump fire back forward forward jump fire back left left right
+//
+// we can see that the sequence "jump fire back" is repeated two times.
+
+exports.botProtection = function(data) {
+    //TODO implement me
+    console.log("data.length")
+    for(let i = 0; i < data.length - 3; i++) {
+      let initCommands = data.slice(i, i+3).join("")
+      for(let j = 1; j < data.length - 3; j++) {
+      const actCommands = data.slice(j, j+3).join("")
+        console.log("actCommands: ", actCommands, "initCommands: ", initCommands)
+        if(actCommands == initCommands) return true
+      }
+    }
+    return false
+}
