a2ccd4ff8b82efc80010b538ca090b34279a7b7d
Jorge Cesar <jorge@thecoursekey.com>
2021-09-25 23:19:45 +0200

    added other solution with benchmark but still first better

--- src/02_bot_protection/index.js
+++ src/02_bot_protection/index.js
@@ -16,12 +16,38 @@
 
 exports.botProtection = function(data) {
     //TODO implement me
+    console.time("for")
+    const t1 = performance.now()
     for(let i = 0; i < data.length - 3; i++) {
       let initCommands = data.slice(i, i+3).join("")
       for(let j = 3; j < data.length - 3; j++) {
       const actCommands = data.slice(j, j+3).join("")
-        if(actCommands == initCommands) return true
+        if(actCommands == initCommands) {
+          const t2 = performance.now()
+          console.log("final: ", t2 - t1)
+          console.timeEnd("for")
+          return true}
       }
     }
+    console.timeEnd("for")
     return false
+    // console.time("doWhile")
+    // const t1 = performance.now()
+    // let isBot = false
+    // let index = 0
+    // let indexCompare = index + 3
+    // do {
+    //   const initCommands = data.slice(index, index+3).join("")
+    //   const actCommands = data.slice(indexCompare, indexCompare+3).join("")
+    //   if(initCommands == actCommands) isBot = !isBot
+    //   if(indexCompare < data.length -3) indexCompare++
+    //   else {
+    //     index++
+    //     indexCompare = (index+3)
+    //   }
+    // } while (index < data.length - 3 || !isBot);
+    // const t2 = performance.now()
+    // console.log("final: ", t2-t1)
+    // console.timeEnd("doWhile")
+    // return isBot
 }
